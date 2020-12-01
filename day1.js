13661b4e7892e8e5a0fbcf3c6f164fb53ff83bb5
Ludwig Ward <ludwig.ward@yubico.com>
2020-12-01 11:36:32 +0100

    Reduce time

--- day1.js
+++ day1.js
@@ -10,28 +10,44 @@ fs.readFile('./input_day1.txt', 'utf8' , (err, data) => {
 })
 
 function calculateNumbers1(input) {
+  let found = false;
   inputArray = input.split("\n");
   for (var i = 0; i < inputArray.length; i++) {
     for (var j = 0; j < inputArray.length; j++) {
       if (j !== i) {
         if (parseInt(inputArray[i]) + parseInt(inputArray[j]) == 2020) {
-          console.log(parseInt(inputArray[i])* parseInt(inputArray[j]));
+          console.log("Two number result: ", parseInt(inputArray[i])* parseInt(inputArray[j]));
+          found = true;
           break;
         }
       }
+    }
+    if (found == true) {
+      break;
     }
   }
 }
 
 function calculateNumbers2(input) {
+  let found = false;
   inputArray = input.split("\n");
   for (var i = 0; i < inputArray.length; i++) {
     for (var j = 0; j < inputArray.length; j++) {
       for (var k = 0; k < inputArray.length; k++) {
-        if (parseInt(inputArray[i]) + parseInt(inputArray[j]) + parseInt(inputArray[k]) == 2020) {
-          console.log(parseInt(inputArray[i]) * parseInt(inputArray[j]) * parseInt(inputArray[k]));
+        if (i !== j && i !== k && j !== k) {
+          if (parseInt(inputArray[i]) + parseInt(inputArray[j]) + parseInt(inputArray[k]) == 2020) {
+            found = true;
+            console.log("Three number result: ", parseInt(inputArray[i]) * parseInt(inputArray[j]) * parseInt(inputArray[k]));
+            break;
+          }
         }
       }
+      if (found == true) {
+        break;
+      }
+    }
+    if (found == true) {
+      break;
     }
   }
 }
